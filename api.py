60e8f0b36e7383ec2045be7006500e7ad095bd72
ziomidlo <kziomek33@gmail.com>
2025-02-09 22:09:37 +0100

    read and preproces data from json to dataFrame

--- api.py
+++ api.py
@@ -4,22 +4,28 @@ import json
 
 
 uri = "https://api.football-data.org/v4/matches"
-headers = {'X-Auth-Token': '26d39f32a17044b7a972763541e4a083'}
+headers = {'X-Auth-Token': ''}
 response = requests.get(uri, headers=headers)
 data = response.json()
 with open('data.json', 'w') as f:
    json.dump(data, f)
 
-df = pd.read_json("matches_28_01_2025.json")
-print(df)
-#for match in data['matches']:
-   # print(f"Match: {match['id']}, Home Team: {match['homeTeam']['name']}, Away Team: {match['awayTeam']['name']}")
+with open("data.json", "r") as file:
+   data = json.load(file)
 
-#plDataSet = requests.get("https://api.football-data.org/v4/competitions/PL/matches", headers=headers)
+matches = data['matches']
+structured_data = []
 
-#plData = pd.DataFrame(plDataSet)
-#print(plData)
+for match in matches: 
+   structured_data.append({
+      "competition": match["competition"]["name"],
+      "match_date": match["utcDate"],
+      "home_team": match["homeTeam"]["name"],
+      "away_team": match["awayTeam"]["name"],
+      "home_score": match["score"]["fullTime"].get("home", None),
+      "away_score": match["score"]["fullTime"].get("away", None),
+      "match_status": match["status"]
+   })
 
-
-#GET https://api.football-data.org/v4/competitions/2021/matches
-#GET https://api.football-data.org/v4/matches
+df = pd.DataFrame(structured_data)
+print(df.head())
